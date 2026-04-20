98e56c89a77735219a5aa30c6480d790f1f15efd
andilutfi944-afk <andilutfi944@gmail.com>
2026-04-20 15:29:42 +0700

    Membuat class Barang beserta atribut dan method printData

--- ParadigmaOOP.cpp
+++ ParadigmaOOP.cpp
@@ -3,4 +3,15 @@ using namespace std;
 
 class Barang {
 public:
-};
+    string nama;
+    int jumlah;
+    string kategori;
+    string tanggalProduksi;
+
+    void printData() {
+        cout << "Nama Barang     : " << nama << endl;
+        cout << "Jumlah          : " << jumlah << endl;
+        cout << "Kategori        : " << kategori << endl;
+        cout << "Tanggal Produksi: " << tanggalProduksi << endl;
+    }
+}; //batas class
